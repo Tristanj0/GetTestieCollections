37439722a8e2e1e052412f97f49383bb712c9ff9
tristan <jametri014@gmail.com>
2022-07-23 18:47:30 -0400

    made arraylist test

--- gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
@@ -7,20 +7,32 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 public class ArrayListTest {
-    private ArrayList<String> arrList;
+    private ArrayList<Person> arrList;
+    private Person person1;
+    private Person person2;
+    private Person person3;
+    private Person person4;
+    private Person person5;
 
     @Before
     public void setUp() {
-        this.arrList = new ArrayList<String>();
-        arrList.add("Harry");
-        arrList.add("James");
-        arrList.add("Bob");
-        arrList.add("Kris");
-        arrList.add("Lisa");
+        this.arrList = new ArrayList<Person>();
+        person1 = new Person("Harry", 1990);
+        person2 = new Person("Kris", 1900);
+        person3 = new Person("Sam", 1995);
+        person4 = new Person("Lisa", 1997);
+        person5 = new Person("Beth", 1992);
     }
 
     @Test
     public void arrayListTest() {
         Assert.assertEquals(5, arrList.size());
     }
+
+    @Test
+    public void arrayListTest_Remove() {
+        arrList.remove(person2);
+        Assert.assertEquals(false, arrList.contains(person2));
+    }
+
 }
